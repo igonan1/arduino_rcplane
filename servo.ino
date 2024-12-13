70bdcc5674eeb8c4c139a4c603805fc49505a17d
igonan1 <92363428+igonan1@users.noreply.github.com>
2024-12-13 12:28:35 +0900

    Add files via upload

---
+++ servo.ino
@@ -0,0 +1,65 @@
+int angle=90;
+int delay_angle=map(angle,0,180,-590,1550);//-590~1550=0~180
+int delay_low;
+int delay_low2;
+void setup() {
+  pinMode(12,OUTPUT);
+  if(delay_angle>=1000){
+    delay_low=2000-delay_angle;
+    delay_low2=17;
+  }
+  else{
+    delay_low=1000-delay_angle;
+    delay_low2=18;
+  }
+  cli();//stop interrupts
+//set timer0 interrupt at 2kHz
+  TCCR0A = 0;// set entire TCCR0A register to 0
+  TCCR0B = 0;// same for TCCR0B
+  TCNT0  = 0;//initialize counter value to 0
+  // set compare match register for 2khz increments
+  OCR0A = 124;// = (16*10^6) / (2000*64) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR0A |= (1 << WGM01);
+  // Set CS01 and CS00 bits for 64 prescaler
+  TCCR0B |= (1 << CS01) | (1 << CS00);   
+  // enable timer compare interrupt
+  TIMSK0 |= (1 << OCIE0A);
+
+//set timer1 interrupt at 1Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS10 and CS12 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+
+//set timer2 interrupt at 8kHz
+  TCCR2A = 0;// set entire TCCR2A register to 0
+  TCCR2B = 0;// same for TCCR2B
+  TCNT2  = 0;//initialize counter value to 0
+  // set compare match register for 8khz increments
+  OCR2A = 249;// = (16*10^6) / (8000*8) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR2A |= (1 << WGM21);
+  // Set CS21 bit for 8 prescaler
+  TCCR2B |= (1 << CS21);   
+  // enable timer compare interrupt
+  TIMSK2 |= (1 << OCIE2A);
+
+
+sei();//allow interrupts
+}
+
+void loop() {
+  digitalWrite(12,HIGH);
+  delayMicroseconds(1000+delay_angle);
+  digitalWrite(12,LOW);
+  delayMicroseconds(delay_low);
+  delay(delay_low2);
+}
